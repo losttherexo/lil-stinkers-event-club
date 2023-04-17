482663b2afd29080955b470f9b720fcb4deac3ed
Andre Vargas Roo <a.vargas252@icloud.com>
2023-04-17 11:26:57 -0400

    tables created

--- server/models.py
+++ server/models.py
@@ -10,3 +10,29 @@ class Fan(db.Model):
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     dob = db.Column(db.Date, nullable=False)
+
+    tickets = db.relationship('Ticket', backref='fan')
+
+class Venue(db.Model):
+    __tablename__ = 'venues'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+
+    tickets = db.relationship('Ticket', backref='venue')
+
+
+class Ticket(db.Model):
+    __tablename__ = 'concerts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    age_restriction = db.Column(db.Boolean, nullable=False, default=False)
+
+    fan_id = db.Column(db.Integer, db.ForeignKey('fans.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
+
