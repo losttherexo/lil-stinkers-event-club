c7d89a2ae725d97c66ef1988ce58ae87e6943e5e
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2023-04-16 23:25:31 -0500

    merged 2

--- server/app.py
+++ server/app.py
@@ -3,14 +3,18 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask import Flask, make_response, jsonify, request, session
+from flask_restful import Resource, Api
+from flask_migrate import Migrate
 
 # Local imports
 from config import app, db, api
 from models import User, Recipe
 
 # Views go here!
+class HomePage(Resource):
+    def get(self):
+        return ''
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
