edd71b66cd2b067e57543fa8d35e41b95a3f7b9b
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2023-04-17 05:05:39 -0500

    serializer added

--- server/models.py
+++ server/models.py
@@ -2,9 +2,9 @@ from sqlalchemy_serializer import SerializerMixin
 
 from config import db
 
-class Fan(db.Model):
+class Fan(db.Model, SerializerMixin):
     __tablename__ = 'fans'
-
+    serialize_rules = ('-tickets',)
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
     first_name = db.Column(db.String, nullable=False)
@@ -13,9 +13,9 @@ class Fan(db.Model):
 
     tickets = db.relationship('Ticket', backref='fan')
 
-class Venue(db.Model):
+class Venue(db.Model, SerializerMixin):
     __tablename__ = 'venues'
-
+    serialize_rules = ('-events',)
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     location = db.Column(db.String, nullable=False)
@@ -23,9 +23,9 @@ class Venue(db.Model):
 
     events = db.relationship('Event', backref='venue')
 
-class Event(db.Model):
+class Event(db.Model, SerializerMixin):
     __tablename__ = 'events'
-
+    serialize_rules = ('-tickets',)
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     date = db.Column(db.Date, nullable=False)
@@ -36,7 +36,7 @@ class Event(db.Model):
 
     venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
 
-class Ticket(db.Model):
+class Ticket(db.Model, SerializerMixin):
     __tablename__ = 'tickets'
 
     id = db.Column(db.Integer, primary_key=True)
