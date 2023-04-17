1da728df0ccabe5b5d195197586c91379e1764ec
Hayden Nault <naulth@gmail.com>
2023-04-17 15:32:33 -0400

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+
+import React, {useState, useEffect} from 'react'
 import Home from "./Home"
 import Navbar from "./Navbar"
 import Venues from "./Venues"
 import Tickets from "./Tickets"
+import Login from "./Login"
 import {
     BrowserRouter,
     Routes,
@@ -10,14 +13,34 @@ import {
   } from "react-router-dom";
 
 function App() {
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        fetch("/check_session").then((response) => {
+            if (response.ok) {
+                response.json().then((user) => setUser(user));
+            }
+        });
+    }, []);
+
+    function handleLogin(user) {
+        setUser(user);
+    }
+
+    function handleLogout() {
+        setUser(null);
+    }
+
   return (
     <div className="App">
         <header className="App-header">
-                <Navbar />
+                <Navbar user={user} onLogout={handleLogout} />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/venues" element={<Venues />} />
                     <Route path="/tickets" element={<Tickets />} />
+                    <Route path="/login" element={<Login onLogin={handleLogin} />} />
                     <Route path="*" element={<h1>404 Page Not Found</h1>} />
                 </Routes>
         </header>
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import {NavLink, Link} from 'react-router-dom'
 import './index.css';
 
 
-function Navbar() {
-
+function Navbar({user, onLogout}) {
+    function handleLogout() {
+        fetch("/logout", {
+            method: "DELETE",
+        }).then(() => onLogout());
+    }
     return(
         <nav classname="nav">
             <NavLink className="NavLink" exact to = "./">Home</NavLink>
             <NavLink className="NavLink" to = "/venues">Venues</NavLink>
             <NavLink className="NavLink" to = "/tickets">Tickets</NavLink>
+            {user ? (
+                <div>
+                    <p>Welcome, {user.username}!</p>
+                    <button onClick={handleLogout}>Logout</button>
+                </div>
+            ) : (
+                <Link to="/login">Click Here to Login</Link>
+            )}
         </nav>
     )
 }
