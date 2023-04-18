071a5876b57062eb9457cf691f6b93e8a350e4fd
Hayden Nault <naulth@gmail.com>
2023-04-18 10:37:04 -0400

    edit form

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Profile({user}){
+
+    const [username, setUsername] = useState("");
+//  const [password, setPassword] = useState("");
+//  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [dob, setDob] = useState("");
+
+    
+
+
     return(
         <div class="mx-auto max-w-2xl text-center justify-between gap-x-6 p-6 lg:px-8">
             <h2 class="text-3xl font-bold tracking-tight text-slate-900 sm:text-5xl">Welcome {user.username}</h2>
+
+            <div>
+                <form class="mx-auto mt-16 max-w-sm sm:mt-20">
+                    <div class="mx-auto max-w-2xl text-center justify-between gap-x-6 p-6 lg:px-8">
+                        <h2 class="text-3xl font-bold tracking-tight text-slate-900 sm:text-5xl">Edit Profile</h2>
+                    </div>
+                    <label for="username">Username:</label>
+                    <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        placeholder={user.username}
+                        autoComplete="off"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        class="block w-full rounded-md border-0 px-3.5 py-2 text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:text-sm sm:leading-6"
+                    />
+        {/* <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+        />
+        <label htmlFor="password">Password Confirmation</label>
+        <input
+          type="password"
+          id="password_confirmation"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+          autoComplete="current-password"
+        /> */}
+                    <label for="first-name">First Name: </label>
+                    <input
+                        type="text"
+                        name="first-name"
+                        id="first-name"
+                        placeholder={user.first_name}
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        class="block w-full rounded-md border-0 px-3.5 py-2 text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:text-sm sm:leading-6"
+                    />
+                    <label for="last-name">Last Name: </label>
+                    <input
+                        type="text"
+                        name="last-name"
+                        id="last-name"
+                        placeholder={user.last_name}
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        class="block w-full rounded-md border-0 px-3.5 py-2 text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:text-sm sm:leading-6"
+                    />
+                    <label for="dob">Date of Birth: </label>
+                    <input
+                        type="date"
+                        id="dob"
+                        name="dob"
+                        placeholder={user.dob}
+                        value={dob}
+                        onChange={(e) => setDob(e.target.value)}
+                        class="block w-full rounded-md border-0 px-3.5 py-2 text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:text-sm sm:leading-6"
+                    />
+                    <div class="mt-10">
+                        <button type="submit" class="block w-full rounded-md bg-slate-900 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-300">Submit</button>
+                    </div>
+                </form>
+            </div>
+
         </div>
     )
 }
--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -86,6 +86,7 @@ function SignUp({ setUser }) {
           type="date"
           id="dob"
           name="dob"
+          placeholder={dob}
           value={dob}
           onChange={(e) => setDob(e.target.value)}
           class="block w-full rounded-md border-0 px-3.5 py-2 text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:text-sm sm:leading-6"
@@ -95,36 +96,6 @@ function SignUp({ setUser }) {
             </div>
       </form>
     </div>
-
-/* <div>
-        
-<form onSubmit={handleSubmit} class="mx-auto mt-16 max-w-sm sm:mt-20">
-    <div class="mx-auto max-w-2xl text-center justify-between gap-x-6 p-6 lg:px-8">
-        <h2 class="text-3xl font-bold tracking-tight text-slate-900 sm:text-5xl">Login</h2>
-    </div>
-    <div>
-        <label for="username" class="block text-sm font-semibold leading-6 text-gray-900">Username</label>
-        <input
-            type="text"
-            name="username"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            class="block w-full rounded-md border-0 px-3.5 py-2 text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:text-sm sm:leading-6"
-        />
-    </div>
-
-    <div class="mt-10">
-        <button type="submit" class="block w-full rounded-md bg-slate-900 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-300">Login</button>
-    </div>
-    <div class="mx-auto flex items-center   gap-x-2">
-        <p>Don't have a username?</p>
-        <Link className="signupbtn" to="/signup">Sign Up!</Link>
-    </div>
-    
-</form>
-
-</div> */
   );
 }
 
