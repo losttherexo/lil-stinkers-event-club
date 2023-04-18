86dc171ee5cfb7c2de856f32dab29fcdf0d13ff3
Hayden Nault <naulth@gmail.com>
2023-04-18 11:20:32 -0400

    profile patch

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,15 +1,34 @@
 import React, {useState} from 'react'
 
-function Profile({user}){
+function Profile({user, setUser}){
 
     const [username, setUsername] = useState("");
 //  const [password, setPassword] = useState("");
 //  const [passwordConfirmation, setPasswordConfirmation] = useState("");
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
-    const [dob, setDob] = useState("");
+//  const [dob, setDob] = useState("");
 
-    
+    const handleEdit = (e) => {
+        e.preventDefault()
+        fetch(`http://localhost:5555/fans/${user.id}` , {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            method: "PATCH",
+            body: JSON.stringify({
+                // password,
+                // password_confirmation: passwordConfirmation,
+                first_name: firstName,
+                last_name: lastName
+            }),
+        }).then((r) => {
+            if (r.ok) {
+              r.json().then((user) => setUser(user));
+            }
+        });
+    }
 
 
     return(
@@ -17,11 +36,11 @@ function Profile({user}){
             <h2 class="text-3xl font-bold tracking-tight text-slate-900 sm:text-5xl">Welcome {user.username}</h2>
 
             <div>
-                <form class="mx-auto mt-16 max-w-sm sm:mt-20">
+                <form onSubmit={handleEdit} class="mx-auto mt-16 max-w-sm sm:mt-20">
                     <div class="mx-auto max-w-2xl text-center justify-between gap-x-6 p-6 lg:px-8">
                         <h2 class="text-3xl font-bold tracking-tight text-slate-900 sm:text-5xl">Edit Profile</h2>
                     </div>
-                    <label for="username">Username:</label>
+                    {/* <label for="username">Username:</label>
                     <input
                         type="text"
                         name="username"
@@ -31,7 +50,7 @@ function Profile({user}){
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                         class="block w-full rounded-md border-0 px-3.5 py-2 text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:text-sm sm:leading-6"
-                    />
+                    /> */}
         {/* <label htmlFor="password">Password</label>
         <input
           type="password"
@@ -68,7 +87,7 @@ function Profile({user}){
                         onChange={(e) => setLastName(e.target.value)}
                         class="block w-full rounded-md border-0 px-3.5 py-2 text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:text-sm sm:leading-6"
                     />
-                    <label for="dob">Date of Birth: </label>
+                    {/* <label for="dob">Date of Birth: </label>
                     <input
                         type="date"
                         id="dob"
@@ -77,7 +96,7 @@ function Profile({user}){
                         value={dob}
                         onChange={(e) => setDob(e.target.value)}
                         class="block w-full rounded-md border-0 px-3.5 py-2 text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:text-sm sm:leading-6"
-                    />
+                    /> */}
                     <div class="mt-10">
                         <button type="submit" class="block w-full rounded-md bg-slate-900 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-300">Submit</button>
                     </div>
