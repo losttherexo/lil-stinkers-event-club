2841db70a3c44a27fb87eeafa2d4d2351b25be57
Hayden Nault <naulth@gmail.com>
2023-04-18 12:06:54 -0400

    profile delete

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { Navigate } from 'react-router-dom';
+import { Navigate, Link } from 'react-router-dom';
 
 function Profile({user, setUser}){
     
@@ -41,6 +41,11 @@ function Profile({user, setUser}){
         });
     }
 
+    const handleDelete = (e) => {
+        fetch(`http://localhost:5555/fans/${user.id}`,{
+        method: 'DELETE'
+        })
+    }
 
     return(
         <div class="mx-auto max-w-2xl text-center justify-between gap-x-6 p-6 lg:px-8">
@@ -113,7 +118,9 @@ function Profile({user, setUser}){
                     </div>
                 </form>
             </div>
-
+            <div class="mt-10">
+                        <button onClick={handleDelete} class="block w-full rounded-md bg-amber-300 px-3.5 py-2.5 text-center text-sm font-semibold text-slate-950 shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-950"><Link to="./">Delete User</Link></button>
+            </div>
         </div>
     )
 }
